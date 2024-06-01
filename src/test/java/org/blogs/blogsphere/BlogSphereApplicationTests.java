84a7be9981c49f7badfcb55a5e6ad98f8d5b2b93
Thescattypotty <bnsyahya123@gmail.com>
2024-06-01 19:45:50 +0100

    Init Project

---
+++ src/test/java/org/blogs/blogsphere/BlogSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blogs.blogsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
