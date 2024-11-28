aa8652e8b15cf789dc85cf7d8275248a3ede118c
Thescattypotty <bnsyahya123@gmail.com>
2024-11-28 21:21:00 +0100

    Initial Project

---
+++ src/main/java/org/blogsphere/blog/BlogSphereApplication.java
@@ -0,0 +1,13 @@
+package org.blogsphere.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/blogsphere/blog/BlogSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blogsphere.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
